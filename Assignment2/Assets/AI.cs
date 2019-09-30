3ab42d394679a15fbda31a349275dadad7c4b454
Nathanial Cowley <nmc160230@utdallas.edu>
2019-09-30 13:19:23 -0500

    Assignment 2 Commit from UTD

---
+++ Assignment2/Assets/AI.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemy one, two, three;
+        one = new Enemy();
+        two = new Enemy();
+        three = new Enemy();
+
+        Debug.Log(one + "\n" + two + "\n" + three);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+class Enemy
+{
+    private string name;
+    private float health, armor, damage;
+    private static long numEnemy = 0;
+
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+
+        set
+        {
+            name = value;
+        }
+    }
+    
+    public float Health
+    {
+        get
+        {
+            return health;
+        }
+
+        set
+        {
+            health = value;
+        }
+    }
+
+    public float Armor
+    {
+        get
+        {
+            return armor;
+        }
+
+        set
+        {
+            armor = value;
+        }
+    }
+
+    public float Damage
+    {
+        get
+        {
+            return damage;
+        }
+
+        set
+        {
+            damage = value;
+        }
+    }
+
+    public Enemy()
+    {
+        name = "Enemy" + numEnemy++.ToString();
+        health = 100f * (float)numEnemy;
+        armor = 50f * (float)numEnemy;
+        damage = 75f * (float)numEnemy;
+    }
+
+    public Enemy(string name, float health, float armor, float damage)
+    {
+        numEnemy++;
+        this.name = name;
+        this.health = health;
+        this.armor = armor;
+        this.damage = damage;
+    }
+
+    public override string ToString()
+    {
+        return "Name: " + name + ", Health: " + health + ", Armor: " + armor + ", Damage: " + damage;
+    }
+}
---
+++ Assignment2/Assets/CubeSphereScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSphereScript : MonoBehaviour
+{
+	public Color color = Color.black;
+	public Vector3 direction = Vector3.left;
+
+    public string nameOfObject = "Default";
+
+    private GameObject self;
+
+    // variables with prefix 'o' are for object,
+    private float oWidth, oHeight;
+    private float maxX, maxY, minX, minY;
+
+    // if you want the objects to move at a different speed, adjust this
+    private float updateSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer myrend = GetComponent<Renderer>();
+        myrend.material.color = color;
+        self = GameObject.Find(nameOfObject);
+        //RectTransform r = (RectTransform)self.transform;
+        oWidth = myrend.bounds.size.x / 2f;
+        oHeight = myrend.bounds.size.y / 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 worldDim = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 10));
+
+        maxX = worldDim.x;
+        maxY = worldDim.y;
+        minX = -maxX;
+        minY = -maxY;
+
+        float xSpeed, ySpeed;
+        xSpeed = maxX * 2f / 10f;
+        ySpeed = maxY * 2f / 10f;
+
+        Vector3 update = self.transform.position;
+
+        // check to see if we're moving vertically
+        if(direction.x == 0)
+        {
+            update += direction * Time.deltaTime * updateSpeed * ySpeed;
+            if(update.y + oHeight > maxY || update.y - oHeight < minY)
+            {
+                direction *= -1f;
+            }
+        }
+        // else, we're moving horizontally
+        else
+        {
+            update += direction * Time.deltaTime * updateSpeed * xSpeed;
+            if(update.x + oWidth > maxX || update.x - oWidth < minX)
+            {
+                direction *= -1f;
+            }
+        }
+
+        self.transform.position = update;
+    }
+}
